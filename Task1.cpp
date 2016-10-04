3623581ca4add518095cc382291dbec6604b4078
Uzair Zia <uzairzia@users.noreply.github.com>
2016-10-05 00:18:17 +0500

    Add files via upload

---
+++ Task1.cpp
@@ -0,0 +1,39 @@
+////Name	: Uzair Zia 
+////Class	: BESE-6A
+////Reg No: 127997
+////Date	: 30 / 09 / 2016
+////Lab	: 2
+////Task	: 1
+//
+////including the libraries
+//#include<iostream>
+//#include<time.h>
+//using namespace std;
+//
+////my function which uses the rand fucntion and a loop to simulate dice rolling
+//void rollDice(int times)
+//{
+//	cout << "\nResult of Dice Roll: ";
+//	for (int i = 0; i < times; i++)
+//	{
+//		//rand() returns a number from 0 to 5 and 1 is added to that returned value
+//		cout << (rand() % 6) + 1 << " ";
+//	}
+//
+//	cout << endl;
+//}
+//
+//int main()
+//{
+//	//seeding the rand function
+//	srand(time(NULL));
+//
+//	int timesOfRoll;
+//
+//	//prompts user and takes input
+//	cout << "How many times do you want to roll the dice: ";
+//	cin >> timesOfRoll;
+//
+//	//calls the dice rolling function
+//	rollDice(timesOfRoll);
+//}
